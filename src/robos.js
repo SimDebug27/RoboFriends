aeeaa630190f8ce509e3f29f2d7ec5552c571ed2
SimDebug27 <simonneyt@hotmail.com>
2020-03-26 19:21:13 +0100

    React RobotFriends

---
+++ src/robos.js
@@ -0,0 +1,44 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Gray',
+        username: 'Antonette',
+        email: 'Shanna@april.test'
+    },
+    {
+        id: 3,
+        name: 'Patrick Star',
+        username: 'Star',
+        email: 'Star@Pat.rick'
+    },
+    {
+        id: 4,
+        name: 'Test Johnny',
+        username: 'Tester',
+        email: 'Test@Jonnhy.biz'
+    },
+    {
+        id: 5,
+        name: 'Who What',
+        username: 'Say',
+        email: 'Say@What.who'
+    },
+    {
+        id: 6,
+        name: 'Damn',
+        username: 'Damn',
+        email: 'Damn@Damn.Daaaaaamn'
+    },
+    {
+        id: 5,
+        name: 'Informer What',
+        username: 'Informer',
+        email: 'Informer@lickie.boom'
+    }
+]
