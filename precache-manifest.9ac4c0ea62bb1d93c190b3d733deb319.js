bbf9a7ea43448a3589c3cf7eb3238efbab8fddeb
SimDebug27 <simonneyt@hotmail.com>
2020-03-27 17:43:58 +0100

    Updates

---
+++ precache-manifest.9ac4c0ea62bb1d93c190b3d733deb319.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1eaf34a7b51e83afd10afef547ce5b",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "fbd74630ded5a74830b5",
+    "url": "/RoboFriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "50ad6b3d3cb3d1fddb02",
+    "url": "/RoboFriends/static/css/main.ec1fc37f.chunk.css"
+  },
+  {
+    "revision": "fbd74630ded5a74830b5",
+    "url": "/RoboFriends/static/js/2.836001f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.836001f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50ad6b3d3cb3d1fddb02",
+    "url": "/RoboFriends/static/js/main.59d64d51.chunk.js"
+  },
+  {
+    "revision": "f5102f4ddabf98e1c227",
+    "url": "/RoboFriends/static/js/runtime-main.77e238d1.js"
+  }
+]);
